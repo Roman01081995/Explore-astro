e57e264b3911a2d1950e04e5db27be1e0021e156
Roman Surzhan <romeo01081995@gmail.com>
2024-08-10 01:40:55 +0200

    Add files via upload

---
+++ explore-astro/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach(card => {
+        card.addEventListener('mouseenter', () => {
+            card.classList.add('active');
+        });
+
+        card.addEventListener('mouseleave', () => {
+            card.classList.remove('active');
+        });
+    });
+});
